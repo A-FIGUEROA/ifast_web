c0d1bf28af7052a9c631f8a693a13d670501c564
RAF-PC <anghelo22899@gmail.com>
2026-01-30 00:45:38 -0500

    dash

--- test_dashboard.php
+++ test_dashboard.php
@@ -1,5 +1,8 @@
 <?php
 // Test para ver qué datos devuelven las funciones
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once 'config/database.php';
 require_once 'includes/auth.php';
 require_once 'includes/functions.php';
